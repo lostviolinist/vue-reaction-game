8ef4a724c0ada062a24287f6c75a9c00bd6d2cac
joyce <lostviolinist@gmail.com>
2021-01-03 23:30:58 +0800

    add button and reaction block

---
+++ src/components/Block.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="block">
+      Click here
+  </div>
+</template>
+
+<script>
+export default {
+    props: ['delay']
+}
+</script>
+
+<style>
+    .block {
+        width: 400px;
+        border-radius: 20px;
+        background: green;
+        color: white;
+        text-align: center;
+        padding: 100px 0;
+        margin: 40px auto;
+    }
+</style>
