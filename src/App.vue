1fc5587ed2d089ed4f129eaa516675fff65e970f
joyce <lostviolinist@gmail.com>
2021-01-04 00:02:22 +0800

    add timer, rank, and style

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,34 @@
 <template>
   <h1>Reaction Timer</h1>
   <button @click="start" :disabled="isPlaying">Play!</button>
-  <Block v-if="isPlaying" :delay="delay" />
+  <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+  <Results v-if="showResults" :score="score" />
 </template>
 
 <script>
 import Block from './components/Block'
+import Results from './components/Results'
 export default {
   name: "App",
-  components: { Block },
+  components: { Block, Results },
   data () {
     return {
       isPlaying: false,
-      delay: null
+      delay: null,
+      score: null,
+      showResults: false
     }
   },
   methods: {
     start() {
       this.isPlaying = true
       this.delay = 2000 + Math.random() * 5000
+      this.showResults = false
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime
+      this.isPlaying = false
+      this.showResults = true
     }
   }
 };
---
+++ src/components/Results.vue
@@ -0,0 +1,33 @@
+<template>
+  <p>Reaction time: {{score}} ms </p>
+  <p class="ranks"> {{rank}} </p>
+</template>
+
+<script>
+export default {
+    props: ['score'],
+    data() {
+        return {
+            rank: null
+        }
+    },
+    mounted() {
+        if(this.score < 250) {
+            this.rank = 'Fire fingers!'
+        } else if (this.score < 400) {
+            this.rank = 'Amazing!'
+        } else {
+            this.rank = 'A little slow...'
+        }
+    }
+}
+</script>
+
+<style>
+    .ranks {
+        font-size: 1.4em;
+        color: green;
+        font-weight: bold;
+    }
+
+</style>
